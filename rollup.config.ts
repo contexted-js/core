b06863bfaa8cf42ae4befe8e0e480f36e0ce0d04
farnik <farnik1995@gmail.com>
2021-12-08 22:39:22 +0330

    Rollup license fix

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,16 +1,30 @@
 import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import license from 'rollup-plugin-license';
 
-export default {
-	input: './src/index.ts',
-	plugins: [typescript()],
-	output: [
-		{
-			file: 'dist/index.cjs',
-			format: 'cjs',
-		},
-		{
-			file: 'dist/index.mjs',
-			format: 'es',
-		},
-	],
-};
+import { resolve } from 'path';
+import { readFile } from 'fs/promises';
+
+async function generateConfiguration() {
+	const banner = await readFile(resolve(__dirname, 'LICENSE'), 'utf-8');
+	return {
+		input: './src/index.ts',
+		plugins: [
+			typescript(),
+			terser({ format: { comments: false } }),
+			license({ banner }),
+		],
+		output: [
+			{
+				file: 'dist/index.cjs',
+				format: 'cjs',
+			},
+			{
+				file: 'dist/index.mjs',
+				format: 'es',
+			},
+		],
+	};
+}
+
+export default generateConfiguration();
